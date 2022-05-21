e2e2a8910988968c8f158104bb59cbc18c00b8d6
Artem-Mv <91866848+Artem-Mv@users.noreply.github.com>
2022-05-21 19:51:22 +0700

    Update CardTests.java

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -7,15 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CardTests {
     @Test
     void cardOrderPositiveTest(){
+
         open("http://localhost:9999/");
 
         $x("//input[@class='input__control']").setValue("Имя Фамилия");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
         $x("//span[@class='button__text']").click();
-        
         boolean actualResult = $x("//*[contains(text(), 'успешно')]").isDisplayed();
-        
+
         assertTrue(actualResult);
     }
 }
