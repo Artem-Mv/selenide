c8288982577267c2fcb8836819574edc688e7a4f
Artem-Mv <91866848+Artem-Mv@users.noreply.github.com>
2022-05-21 19:56:01 +0700

    Update CardTests.java

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -6,15 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CardTests {
     @Test
     void cardOrderPositiveTest(){
-
         open("http://localhost:9999/");
-Thread.sleep(200);
         
         $x("//input[@class='input__control']").setValue("Имя Фамилия");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
         $x("//span[@class='button__text']").click();
-Thread.sleep(200);
+
         boolean actualResult = $x("//*[contains(text(), 'успешно')]").isDisplayed();
 
         assertTrue(actualResult);
