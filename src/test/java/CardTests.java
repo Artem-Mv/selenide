e2700cfb16dc5a6e4e811ed11d6711319f19d067
Artem-Mv <Stimul113@gmail.com>
2022-05-21 18:59:57 +0700

    init

---
+++ src/test/java/CardTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CardTests {
+    @Test
+    void cardOrderPositiveTest(){
+        open("http://localhost:9999/");
+
+        $x("//input[@class='input__control']").setValue("Имя Фамилия");
+        $x("//input[@name='phone']").setValue("+79012345678");
+        $x("//span[@class='checkbox__box']").click();
+        $x("//span[@class='button__text']").click();
+        boolean actualResult = $x("//*[contains(text(), 'успешно')]").isDisplayed();
+
+        assertTrue(actualResult);
+    }
+}
