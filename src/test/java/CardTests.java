e6fd6dff88feb4e9445f0f2e156efef9b4f34c96
Artem-Mv <91866848+Artem-Mv@users.noreply.github.com>
2022-05-24 09:46:19 +0700

    fix assert

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardTests {
     @Test
@@ -12,8 +11,6 @@ public class CardTests {
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
         $x("//span[@class='button__text']").click();
-        boolean actualResult = $x("//*[contains(text(), 'успешно')]").isDisplayed();
-
-        assertTrue(actualResult);
+        $x("//*[contains(text(), 'успешно')]").shouldBe(Condition.visible);
     }
 }
